fa7f1e1fded55fbbb5ccbcea7acae462144305e5
shibowen <shibowen@dangdang.com>
2018-11-05 15:07:51 +0800

    read excel

---
+++ ExcelUtil/src/main/java/com/utils/ExcelUtils/ExcelUtil.java
@@ -0,0 +1,43 @@
+package com.utils.ExcelUtils;
+
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+public class ExcelUtil {
+    private static final Logger log = LoggerFactory.getLogger(ExcelUtil.class);
+    @Test
+    public void readExcel(){
+        try{
+            Workbook workbook=WorkbookFactory.create(new File("E://二期库存.xlsx"));
+            int numberOfSheets = workbook.getNumberOfSheets();
+            for (int i= 0;i<numberOfSheets;i++){
+                Sheet sheet = workbook.getSheetAt(i);
+                int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
+                int lastRowNum = sheet.getLastRowNum();
+                 for(int j=0;i<lastRowNum;i++){
+                     Row row = sheet.getRow(j);
+                     if(row==null){
+                         continue;
+                     }
+                     for(int n=0;n<row.getLastCellNum();n++){
+                         Cell cell = row.getCell(n);
+                         cell.setCellType(Cell.CELL_TYPE_STRING);
+                         String stringCellValue = cell.getStringCellValue();
+                          /*
+                     todo
+                      */
+                     }
+                 }
+            }
+        }catch (Exception e){
+            log.error("读excel文件错误",e);
+        }
+    }
+
+    public void writeExcel(){
+
+    }
+}
